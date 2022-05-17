65adce61decf0b3ca743cac721460972a10f4f41
GCM <g.meccariello@hotmail.com>
2022-05-17 12:30:50 +0200

    barplots

--- analysis.R
+++ analysis.R
@@ -11,6 +11,11 @@ library(zoo)
 library(xts)
 library(tseries)
 
+if(!require('YRmisc')) {
+  install.packages('YRmisc')
+  library('YRmisc')
+}
+
 # loading data from yahoo
 data <- NULL
 tickers <- c('^GSPC', 'AGG', 'BTC-USD')
@@ -39,7 +44,7 @@ sp500.ts <- ts(data$`S&P500`, start=c(2014,9), frequency = 52)
 agg.ts <- ts(data$AGG, start=c(2014,9), frequency = 52)
 btc.ts <- ts(data$`BTC-USD`, start=c(2014,9), frequency = 52)
 
-# decompositon of data
+# decomposition of data
 par(mfrow = c(1,1))
 sp500.dec <- decompose(sp500.ts)
 plot(sp500.dec)
@@ -63,7 +68,7 @@ agg.adf$p.value
 btc.adf <- adf.test(diff(log(btc.ts)))
 btc.adf$p.value
 
-# differantiation and log transformation
+# differentiation and log transformation
 plot(sp500.ts, main='S&P500')
 plot(log(sp500.ts), main='Log transformed S&P500')
 plot(diff(log(sp500.ts)), main='Differentiation of log transformed S&P500')
@@ -96,26 +101,40 @@ agg.btc.cor # no correlation --> 0.044
 
 # return correlations of crypto assets (e.g bitcoin, Ether) and traditional assets (e.g stocks, bonds) change over time and 
 # whether they differ in rising and falling markets (of traditional assets) --> upside and downside of betas of a regression of crypto return on different traditional assets.
+# beta https://www.rdocumentation.org/packages/YRmisc/versions/0.1.6/topics/pt.dbeta
+# high beta implies higher risk with higher reward while low beta implies lower risk with lower reward (at least that is the theory).
+# (e.g. a stock with a beta of 2.00 will gain twice as much as the market on up days and lose twice as much as the market on down days)
+
+# pt.dbeta(ar,mr,rf) .. ar: a vector of a risk asset return, mr:a vector of market return, rf:risk free rate
+
+# assuming market return is given by the S&P500
+btc.sp500.betas <- pt.dbeta(log_returns[,3], log_returns[,1], 0)
+btc.sp500.betas
+
+
+# assuming market return is given by the AGG
+btc.agg.betas <- pt.dbeta(log_returns[,3], log_returns[,2], 0)
+btc.agg.betas
+
+
+sp500.agg.betas <- pt.dbeta(log_returns[,1],log_returns[,2],0)
+sp500.agg.betas$upbeta
+sp500.agg.betas$downbeta
+
+sp500.btc.betas <- pt.dbeta(log_returns[,1],log_returns[,3],0)
+sp500.btc.betas$upbeta
+sp500.btc.betas$downbeta
+
+agg.btc.betas <- pt.dbeta(log_returns[,2],log_returns[,3],0)
+agg.btc.betas$upbeta
+agg.btc.betas$downbeta
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+counts <- c(btc.sp500.betas$upbeta, btc.sp500.betas$downbeta)
+barplot(counts, main="Beta values",
+        xlab="BTC", col=c("red","green"),
+        legend = rownames(counts), beside=TRUE)
 
 
 
